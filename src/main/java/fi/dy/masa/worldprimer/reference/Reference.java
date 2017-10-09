730b3f30d8880bc22b5a7a24c10b13da2d070b11
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-10-09 05:02:50 +0300

    Sign the JAR

--- src/main/java/fi/dy/masa/worldprimer/reference/Reference.java
+++ src/main/java/fi/dy/masa/worldprimer/reference/Reference.java
@@ -5,6 +5,7 @@ public class Reference
     public static final String MOD_ID = "worldprimer";
     public static final String MOD_NAME = "World Primer";
     public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
 
     public static final String PROXY_CLIENT = "fi.dy.masa.worldprimer.proxy.ClientProxy";
     public static final String PROXY_SERVER = "fi.dy.masa.worldprimer.proxy.CommonProxy";
