3dacb77bb5b1fd68f24be5206a352a9e10f7a4a5
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-10-09 05:02:50 +0300

    Sign the JAR

--- src/main/java/fi/dy/masa/worldprimer/WorldPrimer.java
+++ src/main/java/fi/dy/masa/worldprimer/WorldPrimer.java
@@ -7,6 +7,7 @@ import net.minecraft.world.chunk.storage.AnvilSaveConverter;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
@@ -16,7 +17,7 @@ import fi.dy.masa.worldprimer.proxy.IProxy;
 import fi.dy.masa.worldprimer.reference.Reference;
 import fi.dy.masa.worldprimer.util.DimensionLoadTracker;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.worldprimer.config.WorldPrimerGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/worldprimer/master/update.json",
     acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.10,1.11.2]")
@@ -105,6 +106,25 @@ public class WorldPrimer
         else
         {
             logger.debug(message, params);
+        }
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
         }
     }
 }
